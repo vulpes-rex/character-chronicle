8a2e0e537f79cc92a8418ef046aeb85ba91b1af3
Kevin V <kvwulp@gmail.com>
2025-05-01 20:10:24 +0000

    make deployable as a firebase application

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,7 @@ const nextConfig: NextConfig = {
      // For client-side Firebase SDK (firebase/app, firebase/firestore), this usually isn't needed.
      // serverComponentsExternalPackages: ['@google-cloud/firestore'], // Example
    },
+   output: 'standalone', // Optimize for deployment environments
 };
 
 export default nextConfig;
